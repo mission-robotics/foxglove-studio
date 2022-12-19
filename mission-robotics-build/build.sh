1aa1a007dbebbc1338edb0035db5bc552986f998
Dominik Fretz <dominik.fretz@gmail.com>
2022-12-19 14:37:30 +1100

    Make build work

--- mission-robotics-build/build.sh
+++ mission-robotics-build/build.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
-cat <<EOF >> .yarnrc.yml
-npmScopes:
-  foxglove:
-    npmRegistryServer: 'https://gitlab.com/api/v4/projects/${CI_PROJECT_ID}/packages/npm/'
-    npmPublishRegistry: 'https://gitlab.com/api/v4/projects/${CI_PROJECT_ID}/packages/npm/'
-    npmAlwaysAuth: true
-    npmAuthToken: '${CI_JOB_TOKEN}'
+#cat <<EOF >> .yarnrc.yml
+#npmScopes:
+#  foxglove:
+#    npmRegistryServer: 'https://gitlab.com/api/v4/projects/${CI_PROJECT_ID}/packages/npm/'
+#    npmPublishRegistry: 'https://gitlab.com/api/v4/projects/${CI_PROJECT_ID}/packages/npm/'
+#    npmAlwaysAuth: true
+#    npmAuthToken: '${CI_JOB_TOKEN}'
+#
+#EOF
 
-EOF
 
+#corepack enable
+#yarn install
 
-corepack enable
-yarn install
+echo "Building log"
+yarn workspace @foxglove/log prepack
+echo "Building hooks"
+yarn workspace @foxglove/hooks prepack
+echo "Building den"
+yarn workspace @foxglove/den prepack
 
+echo "Building packages"
 yarn build:packages
 
-yarn workspace @foxglove/den npm publish
-yarn workspace @foxglove/hooks npm publish
-yarn workspace @foxglove/log npm publish
-yarn workspace @foxglove/studio-base npm publish
+#yarn workspace @foxglove/den npm publish
+#yarn workspace @foxglove/hooks npm publish
+#yarn workspace @foxglove/log npm publish
+#yarn workspace @foxglove/studio-base npm publish
